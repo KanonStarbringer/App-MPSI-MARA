e037234618815a210bbd066085e4acf1defc9934
Tullio Pires <94844118+KanonStarbringer@users.noreply.github.com>
2023-10-06 20:55:30 -0300

    app.py

--- app.py
+++ app.py
@@ -10,6 +10,13 @@ from reportlab.lib import colors
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Table, TableStyle
 
+# Set the app title and description
+st.set_page_config(
+    page_title="Your App's Title",
+    page_icon=":chart_with_upwards_trend:",  # You can customize the icon
+    layout="wide",  # You can set the layout (wide or center)
+    initial_sidebar_state="auto"  # You can set the initial sidebar state
+)
 
 def get_payoff_matrix():
     num_alternatives = st.number_input("Enter the number of alternatives:", min_value=2, value=2, step=1)
